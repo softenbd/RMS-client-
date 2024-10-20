e1b03ef9a1bb94c368f3dad8cd9bba26b7a3fd3c
promahbubul <mahbublalam500@gmail.com>
2024-10-20 19:40:50 +0600

    Update Admin Dashoard unnessacery import or others deleted

--- src/modules/dashboard/admin-dashboard/dashboard/AdminDashboard.tsx
+++ src/modules/dashboard/admin-dashboard/dashboard/AdminDashboard.tsx
@@ -9,7 +9,6 @@ import {
 } from "lucide-react";
 
 const AdminDashboard = () => {
-  
   return (
     <div className="p-5  min-h-[calc(100vh-40px)] overflow-y-auto max-h-[calc(100vh-40px)] ">
       <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-5 items-stretch">
