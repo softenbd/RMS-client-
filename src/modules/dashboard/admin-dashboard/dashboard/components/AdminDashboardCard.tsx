46a0fdfad3aeda64d7392170174a5e5fbb35f949
promahbubul <mahbublalam500@gmail.com>
2024-10-20 19:16:55 +0600

    Admin Dashboard card type defined

--- src/modules/dashboard/admin-dashboard/dashboard/components/AdminDashboardCard.tsx
+++ src/modules/dashboard/admin-dashboard/dashboard/components/AdminDashboardCard.tsx
@@ -1,30 +1,37 @@
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Salad } from "lucide-react";
-const AdminDashboardCard = ({ title, number, icon: Icon, className }) => {
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import { ElementType } from "react";
+
+interface AdminDashboardCardProps {
+  title: string;
+  number: number;
+  icon?: ElementType;
+  className?: string;
+}
+
+const AdminDashboardCard: React.FC<AdminDashboardCardProps> = ({
+  title,
+  number,
+  icon: Icon,
+  className,
+}) => {
   return (
     <Card
       className={`h-max ${className} cursor-pointer hover:scale-[1.03] duration-500 h-full`}
     >
       <CardHeader>
         <div className="flex flex-row items-center">
-          <div className="">
-            {Icon && <Icon className="text-4xl text-slate-600" size={40} />}
+          <div>
+            {Icon && <Icon size={35} className="text-4xl text-slate-600" />}
+            {/* lucide-react icons use className for styling */}
           </div>
-          <div className=" w-full">
+          <div className="w-full">
             <p className="text-6xl font-semibold text-center">{number}</p>
           </div>
         </div>
         <CardTitle className="pt-14">{title}</CardTitle>
-        {/* <CardDescription>Card Description</CardDescription> */}
       </CardHeader>
     </Card>
   );
 };
+
 export default AdminDashboardCard;
